2cc1769b77c6447bdf71f6ca8f72e2bf5b90be18
Dmitry Shumovsky <d.shumovsky@bk.ru>
2022-03-10 16:18:29 +0300

    add: new smoothbehavior

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,28 +1,41 @@
-const scroll = () => {
-    const nav = document.querySelector('.header__nav_list');
-    nav.addEventListener('click', (e) => {
+// const scroll = () => {
+//     const nav = document.querySelector('.header__nav_list');
+//     nav.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         if (e.target.children) {
+//             let link = e.target;
+//             let id = link.getAttribute('href').substring(1);
+//             const section = document.getElementById(id);
+
+//             if (section) {
+//                 seamless.elementScrollIntoView(section, {
+//                     behavior: 'smooth',
+//                     block: 'start'
+//                 })
+//             } else {
+//                 seamless.elementScrollIntoView(document.querySelector("#target"), {
+//                     behavior: "smooth",
+//                     block: "center",
+//                     inline: "center",
+//                 });
+//             }
+//         }
+
+//     })
+
+// }
+// seamless.polyfill();
+// scroll();
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function(e) {
         e.preventDefault();
-        if (e.target.children) {
-            let link = e.target;
-            let id = link.getAttribute('href').substring(1);
-            const section = document.getElementById(id);
+        const id = smoothLink.getAttribute('href');
 
-            if (section) {
-                seamless.elementScrollIntoView(section, {
-                    behavior: 'smooth',
-                    block: 'start'
-                })
-            } else {
-                seamless.elementScrollIntoView(document.querySelector("#target"), {
-                    behavior: "smooth",
-                    block: "center",
-                    inline: "center",
-                });
-            }
-        }
-
-    })
-
-}
-seamless.polyfill();
-scroll();
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
