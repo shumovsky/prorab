2bc008cf4ed49ff1ce9ac2a47f01063a3bdd6976
Dmitry Shumovsky <d.shumovsky@bk.ru>
2022-03-14 19:24:21 +0300

    feat:change hamburger menu

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,10 +1,12 @@
 const menu = () => {
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('.header__nav');
+    const header = document.querySelector('.header__wrapper');
 
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('nav');
         hamburger.classList.toggle('hamburger_active');
+        header.classList.toggle('header_visible')
     })
 
     nav.addEventListener('click', (e) => {
