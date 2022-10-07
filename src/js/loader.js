90f01fcccc24239b27c4e7e9d9661d1c6ac90e1a
Dmitry Shumovsky <d.shumovsky@bk.ru>
2022-10-07 23:20:38 +0300

    fix: bug localstorage

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => { /* Страница загружена, включая все ресурсы */
     const preloader = document.querySelector('.load') /* находим блок Preloader */
     preloader.classList.add('preloader_hidden') /* добавляем ему класс для скрытия */
+    localStorage.removeItem('prod');
 })
