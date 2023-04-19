38a3b5ec768ed7c860cce744c3be11acde24695f
Dmitry Shumovsky <d.shumovsky@bk.ru>
2023-04-19 21:13:05 +0300

    finaly form

--- mail.php
+++ mail.php
@@ -4,8 +4,31 @@
  $email = $_POST['email'];
  $message = $_POST['message'];
 
- $subject = '=?utf-8?B?'.base64_encode("Сообщение с сайта")'?=';
-  $headers = 'From: $email\r\nReply-to: $email\r\nContent-type: tetx/html; charset=utf-8\r\n'; 
+//  $subject = '=?utf-8?B?'.base64_encode("Сообщение с сайта")'?=';
+//   $headers = 'From: $email\r\nReply-to: $email\r\nContent-type: tetx/html; charset=utf-8\r\n'; 
 
- $saccess = mail('d.shumovsky@bk.ru', $subject, $message, $headers);
+//  $saccess = mail('d.shumovsky@bk.ru', $subject, $message, $headers);
+
+
+//  echo var_dump($_POST);
+
+// $name = $_POST["name"];
+// $city = $_POST["city"];
+// $phone = $_POST["phone"];
+
+// echo var_dump($phone);
+
+
+$to = "d.shumovsky@bk.ru"; /* Адрес, куда отправляем письма*/
+$subject = "Письмо с обратной связи"; /*Тема письма*/
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n";
+$headers .= "From: <test@mail.ru>\r\n";/*ОТ КОГО*/
+
+/*ВО ВНУТРЬ ПЕРЕМЕННОЙ $message ЗАПИСЫВАЕМ ДАННЫЕ ИЗ ПОЛЕЙ */
+$message .= "Имя пользователя: ".$name."\n";
+$message .= "Населенный пункт: ".$city."\n";
+$message .= "Телефон: ".$phone."\n";
+
+mail ($to, $subject, $message, $headers)
 ?>
--- src/js/sendmail.js
+++ src/js/sendmail.js
@@ -1,45 +1,37 @@
-// Form;
-
 const form = () => {
-  const forms = document.querySelector('#form');
-  console.log(forms);
+  const forms = document.querySelector('form');
+
   const message = {
     loading: 'Загрузка',
     success: 'Успешно',
     failure: 'Ошибка',
   };
 
-  forms.forEach((item) => {
-    postData(item);
-  });
+  forms.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  function postData(form) {
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
+    const statusMessage = document.createElement('div');
+    statusMessage.textContent = message.loading;
+    forms.append(statusMessage);
 
-      const statusMessage = document.createElement('div');
-      statusMessage.textContent = message.loading;
-      form.append(statusMessage);
+    const recquest = new XMLHttpRequest();
+    recquest.open('POST', 'mail.php');
 
-      const recquest = new XMLHttpRequest();
-      recquest.open('POST', 'mail.php');
+    recquest.setRequestHeader('Contetn-type', 'multipart/form-data');
+    const formData = new FormData(forms);
 
-      recquest.setRequestHeader('Contetn-type', 'multipart/form-data');
-      const formData = new FormData(form);
+    recquest.send(formData);
 
-      recquest.send(formData);
-
-      recquest.addEventListener('load', () => {
-        if (recquest.status == 200) {
-          console.log(recquest.response);
-          statusMessage.textContent = message.success;
-          form.reset();
-        } else {
-          statusMessage.textContent = message.failure;
-        }
-      });
+    recquest.addEventListener('load', () => {
+      if (recquest.status == 200) {
+        console.log(recquest.response);
+        statusMessage.textContent = message.success;
+        forms.reset();
+      } else {
+        statusMessage.textContent = message.failure;
+      }
     });
-  }
+  });
 };
 
 form();
