c8c5d531945b806dbf8ed958ff1072de7db78019
Dmitry Shumovsky <d.shumovsky@bk.ru>
2023-04-19 21:48:48 +0300

    test new func

--- src/js/sendmail.js
+++ src/js/sendmail.js
@@ -35,3 +35,45 @@ const form = () => {
 };
 
 form();
+
+$(function () {
+  $('#contact').submit(function () {
+    var errors = false;
+    $(this).find('span').empty();
+    $(this)
+      .find('input, textarea')
+      .each(function () {
+        if ($.trim($(this).val()) == '') {
+          errors = true;
+          $(this)
+            .next()
+            .text('Не заполнено поле ' + $(this).prev().text());
+        }
+      });
+    if (!errors) {
+      var data = $('#contact').serialize();
+      $.ajax({
+        url: 'letter.php',
+        type: 'POST',
+        data: data,
+        beforeSend: function () {
+          $('#submit').next().text('Отправляю...');
+        },
+        success: function (res) {
+          if (res == 1) {
+            $('#contact').find('input:not(#submit), textarea').val('');
+            $('#submit').next().empty();
+            alert('Письмо отправлено');
+          } else {
+            $('#submit').next().empty();
+            alert('Ошибка отправки');
+          }
+        },
+        error: function () {
+          alert('Ошибка!');
+        },
+      });
+    }
+    return false;
+  });
+});
